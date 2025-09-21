fd3bd71b85a013df065e66f19be4d366c460cf85
yehuda-d <yehuda130@gmail.com>
2025-09-21 20:34:08 +0300

    ייצוא וייבוא של הנתיבים

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+//ספריות
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 4500;
 
+//מידלוורים
 app.use(express.static(__dirname));
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/public/index.html')
-});
+//נתיבים    
+app.get('/',(req,res)=>{ res.sendFile(__dirname + '/public/index.html')});
+app.use('/p',require('./routes/projects_R'))
 
 
+// הפעלת שרת
 app.listen(port,()=>{console.log(`http://localhost:${port}`)});
