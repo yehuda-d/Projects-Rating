b6c8708d0321961b0e96a5ce5bf981ba763f3dce
yehuda-d <yehuda130@gmail.com>
2025-09-21 20:04:13 +0300

    הוספת תקייה ציבורית לקבצי צד לקוח ובדיקה שהקבצים מקושרים

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const port = 4500;
 
+app.use(express.static(__dirname));
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/public/index.html')
 });
---
+++ public/script.js
@@ -0,0 +1,5 @@
+function addTitle(){
+    let txt = "Projects Rating"
+    document.getElementById("h1").innerText = txt;
+}
+addTitle();
