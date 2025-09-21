8afa6143aeee62cabc37d4c2343e6a00f57e4b44
yehuda-d <yehuda130@gmail.com>
2025-09-21 19:48:19 +0300

    הוספת נקודת קצה שמחזירה קובץ html

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 4500;
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+});
 
 
 app.listen(port,()=>{console.log(`http://localhost:${port}`)});
