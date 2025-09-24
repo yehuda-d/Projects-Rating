9d0b23e9d48fad0698f6b8f98c02ee4d23ed554c
yehuda-d <yehuda130@gmail.com>
2025-09-24 21:57:15 +0300

    הוספת מתודת פוסט, התקנת ספריות עזר להעלות ולערוך קבצים, הוספת אובייקט ההוספה ועדכון קבצים

--- public/script.js
+++ public/script.js
@@ -2,4 +2,8 @@ function addTitle(){
     let txt = "Projects Rating"
     document.getElementById("h1").innerText = txt;
 }
+
+
+
 addTitle();
+
--- routes/projects_R.js
+++ routes/projects_R.js
@@ -1,12 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');//ספרייה שנותנת להעלות קבצים
+const fs = require('fs');//ספרייה שנותנת לערוך קבצים
+const path = require('path');
 
 const products = [];
 let nextID = 1;
 
+//אם אין תיקייה כזו אז שתייצר אותה
+if(!fs.existsSync('uploads')){
+    fs.mkdirSync('uploads');
+}
+
+// אובייקט ההוספה ועדכון קבצים
+const storage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null, 'uploads/');
+    },
+    filename: (req,file,cb)=>{
+        let id = req.params.id ? req.params.id : nextID;
+        let finalFilename = `${id}${path.extname(file.originalname)}`;
+        cb(null, finalFilename);
+    }
+})
+const uplode = multer({storage: storage});
+
 router.get('/',(req,res)=>{
     res.status(200).json(products)
 });
+
+router.post('/',uplode.single('myFile'),(req,res)=>{
+    let name = req.body.name;
+    if(!name){
+        return res.status(400).json({message:"not valid"})       
+    }
+    let id = nextID++;
+    let price = parseFloat(req.body.price);
+    let filename = req.file ? req.file.filename : null;
+    let product = {id,name,price:parseFloat(price),filename};
+    products[id] = product;
+     res.status(201).json({message:"ok"})
+
+})
 
 
 module.exports = router;    
