165444b9d8d386f5c4a43e7763d7dd03978e5e45
yehuda-d <yehuda130@gmail.com>
2025-09-24 21:40:38 +0300

    הקמת מערך ומשתנה נקסא איידי ומתודה שמחזירה את המערך

--- routes/projects_R.js
+++ routes/projects_R.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const products = [];
+let nextID = 1;
+
+router.get('/',(req,res)=>{
+    res.status(200).json(products)
+});
+
 
 module.exports = router;    
