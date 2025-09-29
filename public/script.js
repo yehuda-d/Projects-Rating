3998164ff0a76b03986cadacebeed82c939f3f68
yehuda-d <yehuda130@gmail.com>
2025-09-29 12:18:28 +0300

    הוספת פונקציה של טעינת הפרויקט

--- public/script.js
+++ public/script.js
@@ -125,6 +125,18 @@ function addOrEdit(){
     }
 }
 
+let params = new URLSearchParams(window.location.search);
+    let id = params.get("id");
+
+    async function loadProject() {
+      let res = await fetch(`/p/${id}`);
+      let obj = await res.json();
+      document.getElementById("title").innerText = obj.name;
+      document.getElementById("desc").innerText = obj.description;
+      document.getElementById("img").src = "/uploads/" + obj.Myfilename;
+      document.getElementById("ratingInfo").innerText = "דירוג נוכחי: " + (obj.rating || 0).toFixed(2);
+    }
+
 
 getData();
 addTitle();
